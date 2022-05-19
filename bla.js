46e48b2bf7d2e4e64d6c02243ef21c977aab9207
GAD STAMATI <46291220@ort>
2022-05-19 12:13:27 -0300

    19/05/22

--- bla.js
+++ bla.js
@@ -1,13 +1,35 @@
-function CheckInputsbyInput() {
-    let nota=document.getElementsByTagName("input");
+let validar=false;
+
+
+let validarNota = (nota,materia) =>{
+  
     if(nota>=1 && nota <=10){
-        document.getElementsByTagName("input").style.color="green"
-        return true;
+        document.getElementById(materia).style.color="green"
+        validar=false;
     }else{
-        document.getElementsByTagName("input").style.color="red"
-        return false;
+        document.getElementById(materia).style.color="red"
+        validar=true;
     }
-  }
- 
+}
+
+
+
+let calcularPromedio = () =>{
+    var mate=document.getElementById('notaMate').value;
+    var lengua=document.getElementById('notaLengua').value;
+    var Efsi=document.getElementById('notaEfsi').value;
+
+    if(validar){
+        document.getElementById('promedio').value="error";
+    }
+    else{
+        var promedio=(parseFloat(mate)+parseFloat(lengua)+ parseFloat(Efsi))/3;
+    document.getElementById('promedio').value=promedio;
+    }
+    
+    var max = Math.max(mate,lengua,Efsi);
+    document.getElementById('maximo').value=max;
+}
+
 
   
